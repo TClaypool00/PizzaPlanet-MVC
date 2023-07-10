e92de1efa0bbfce4194e6ebff00179bff132c550
TClaypool00 <claypooltyler00@gmail.com>
2023-07-09 22:21:38 -0500

    Created new MVC project

---
+++ PizzaPlanet/PizzaPlanet.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PizzaPlanet.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
