a464a4dd707cf12351ed5261edb203e00deb4ec5
TClaypool00 <claypooltyler00@gmail.com>
2023-07-15 15:46:48 -0500

    Created Attribute to restrict the if they are already logged in

---
+++ PizzaPlanet/PizzaPlanet.App/App_Code/Attributes/NotAuthorize.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace PizzaPlanet.App.App_Code.Attributes
+{
+    public sealed class NotAuthorize : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new UnauthorizedObjectResult("You cannot access this page if you are logged");
+
+                return;
+            }
+        }
+    }
+}
