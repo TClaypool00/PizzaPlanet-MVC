b34648b3ad0559b6711b4818e1c76dc97eb59efb
TClaypool00 <claypooltyler00@gmail.com>
2023-07-15 14:41:16 -0500

    Added ControllerHelper class

---
+++ PizzaPlanet/PizzaPlanet.App/Controllers/ControllerHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaPlanet.App.Controllers
+{
+    public class ControllerHelper : Controller
+    {
+        protected void SetTempError(string error)
+        {
+            TempData["Error"] = error;
+        }
+
+        protected void SetTempSuccess(string message)
+        {
+            TempData["Success"] = message;
+        }
+
+        protected void GetTempError()
+        {
+            if (TempData["Error"] is not null)
+            {
+                ViewBag.error = TempData["Error"];
+            }
+        }
+
+        protected void GetTempSuccess()
+        {
+            if (TempData["success"] is not null)
+            {
+                ViewBag.success = TempData["success"];
+            }
+        }
+
+        protected ActionResult SetError(string error, object model)
+        {
+            ViewBag.error = error;
+
+            return View(model);
+        }
+    }
+}
