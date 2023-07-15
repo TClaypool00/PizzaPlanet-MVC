3939be3d1669632f73faacd3a888fb774f257de4
TClaypool00 <claypooltyler00@gmail.com>
2023-07-15 15:43:30 -0500

    Added Password service

---
+++ PizzaPlanet/PizzaPlanet.App/App_Code/BLL/IPasswordService.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPlanet.App.App_Code.BLL
+{
+    public interface IPasswordService
+    {
+        bool PasswordMeetsRequirements(string password);
+
+        string HashPassword(string password);
+
+        bool ValidatePassword(string password, string hash);
+    }
+}
---
+++ PizzaPlanet/PizzaPlanet.App/App_Code/DAL/PasswordService.cs
@@ -0,0 +1,23 @@
+﻿using PizzaPlanet.App.App_Code.BLL;
+using System.Text.RegularExpressions;
+
+namespace PizzaPlanet.App.App_Code.DAL
+{
+    public class PasswordService : IPasswordService
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool PasswordMeetsRequirements(string password)
+        {
+            return Regex.IsMatch(password, @"^(.{8,20}|[^0-9]*|[^A-Z])$");
+        }
+
+        public bool ValidatePassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
