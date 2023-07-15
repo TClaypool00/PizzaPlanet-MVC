b08952e9ec58ef341f22339b12acdc11c5fdb2ee
TClaypool00 <claypooltyler00@gmail.com>
2023-07-15 14:42:08 -0500

    Added Authorize attribute

--- PizzaPlanet/PizzaPlanet.App/Controllers/HomeController.cs
+++ PizzaPlanet/PizzaPlanet.App/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PizzaPlanet.App.Models;
 using System.Diagnostics;
 
 namespace PizzaPlanet.App.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
