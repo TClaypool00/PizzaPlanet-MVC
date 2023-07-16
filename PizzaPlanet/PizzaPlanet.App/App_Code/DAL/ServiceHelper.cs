9ea4d35ef0fbaa56ff8ab7f7bc0f36cd9865f442
TClaypool00 <claypooltyler00@gmail.com>
2023-07-16 16:07:50 -0500

    Added ServiceHelper class and configureIndex method

---
+++ PizzaPlanet/PizzaPlanet.App/App_Code/DAL/ServiceHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace PizzaPlanet.App.App_Code.DAL
+{
+    public class ServiceHelper
+    {
+        protected int _index;
+
+        protected void ConfigureIndex(int? index)
+        {
+            if (!index.HasValue || index == 0)
+            {
+                _index = 0;
+            }
+            else
+            {
+                _index = 10 * index.Value;
+            }
+        }
+    }
+}
